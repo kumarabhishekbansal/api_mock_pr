27df490a40484f5ea6dfae355734d2fc7110eea3
kumarabhishekbansal <kumarabhishekbansal>
2024-03-18 16:00:59 +0530

    Page not found functionality using connect-api-mocker

--- mocks/api/users/__user_id__/GET.js
+++ mocks/api/users/__user_id__/GET.js
@@ -1,5 +1,15 @@
 module.exports=function(req,res){
-    res.json({
-        "id":req.params.user_id
-    })
+    console.log(__dirname);
+    // var targetFileName='GET.json'
+    var targetFileName = require("./GET.json")
+    var id=req.params.user_id;
+    // console.log(id,typeof(id));
+    let obj=targetFileName.find((val)=>{
+        // console.log(val.id,parseInt(id),typeof(parseInt(id)),typeof(val.id));
+        return val.id===parseInt(id)
+    });
+    console.log(obj);
+    // res.sendFile(targetFileName, {root: __dirname});
+    res.send(obj);
+    // console.log(targetFileName);
 }
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var apiMocker = require('connect-api-mocker');
+var express = require("express");
+var apiMocker = require("connect-api-mocker");
+const { notFound } = require("connect-api-mocker/helpers");
 
 var app = express();
+app.use(notFound("Page is not found"));
+app.use("/api", apiMocker("mocks/api"));
 
-app.use('/api', apiMocker('mocks/api'));
-
-app.listen(8080,()=>{
-    console.log("server is listen at port no. 8080");
+app.listen(8080, () => {
+  console.log("server is listen at port no. 8080");
 });
